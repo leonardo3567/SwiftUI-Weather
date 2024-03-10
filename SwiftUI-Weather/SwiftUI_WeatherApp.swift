2de94dbb5910463e690cabd4d18c6d3f7f60db13
Leonardo Gomes <leonardo35671@hotmail.com>
2024-03-10 15:42:52 +0000

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Newuser on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
